2dd76211e5081d2abcc96611f0251ee6dc43a22e
Declan-J <126972375+Declan-J@users.noreply.github.com>
2025-07-06 23:24:27 +0100

    Human choice prompt

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 // GET COMPUTER CHOICE
 function getComputerChoice() {
-
   randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
-    case 0: return "Rock"
-      break;
-    case 1: return "Paper"
-      break;
-    case 2: return "Scissors"
-      break;
+    case 0: return "Rock";
+    case 1: return "Paper";
+    case 2: return "Scissors";
     default: return console.error("Computer has generated an invalid value");
-      break;
   }
 }
 
+function getHumanChoice() {
+  return prompt("Rock, Paper, or Scissor?");
+
+}
+
 console.log(getComputerChoice());
+console.log(getHumanChoice());
