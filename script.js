d6ef6bca465ed08a3fcd898e9b6ad06e8aee3e1d
Declan-J <126972375+Declan-J@users.noreply.github.com>
2025-07-06 23:28:45 +0100

    Plays a round
    TODO: Winner logic, message and award point

--- script.js
+++ script.js
@@ -2,17 +2,31 @@
 function getComputerChoice() {
   randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
-    case 0: return "Rock";
-    case 1: return "Paper";
-    case 2: return "Scissors";
-    default: return console.error("Computer has generated an invalid value");
+    case 0:
+      return "Rock";
+    case 1:
+      return "Paper";
+    case 2:
+      return "Scissors";
+    default:
+      return console.error("Computer has generated an invalid value");
   }
 }
 
 function getHumanChoice() {
   return prompt("Rock, Paper, or Scissor?");
-
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function play_round() {
+  let humanScore = "0";
+  let computerScore = "0";
+
+  let humanChoice = getHumanChoice();
+  humanChoice = humanChoice.toLowerCase();
+  let computerChoice = getComputerChoice();
+  console.log(humanChoice);
+  console.log(computerChoice);
+}
+
+// LOGS
+console.log(play_round())
