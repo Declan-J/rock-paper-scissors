d60f321635596ea9564a37d48f13721be9eddd40
Declan-J <126972375+Declan-J@users.noreply.github.com>
2025-07-07 16:34:29 +0100

    Completed basic play_round function

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function getComputerChoice() {
   randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
     case 0:
-      return "Rock";
+      return "rock";
     case 1:
-      return "Paper";
+      return "paper";
     case 2:
-      return "Scissors";
+      return "scissors";
     default:
       return console.error("Computer has generated an invalid value");
   }
@@ -18,15 +18,37 @@ function getHumanChoice() {
 }
 
 function play_round() {
-  let humanScore = "0";
-  let computerScore = "0";
+  let humanScore = 0;
+  let computerScore = 0;
 
   let humanChoice = getHumanChoice();
   humanChoice = humanChoice.toLowerCase();
   let computerChoice = getComputerChoice();
   console.log(humanChoice);
   console.log(computerChoice);
+
+  // HUMAN WINS
+  if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log("Winner: Player");
+    humanScore++;
+
+    //CPU WINS
+  } else if (
+    (computerChoice === "rock" && humanChoice === "scissors") ||
+    (computerChoice === "paper" && humanChoice === "rock") ||
+    (computerChoice === "scissors" && humanChoice === "paper")
+  ) {
+    console.log("Winner: Computer");
+    computerScore++;
+  }
+
+  // DRAW
+  else console.log("It's a Draw!");
 }
 
 // LOGS
-console.log(play_round())
+console.log(play_round());
